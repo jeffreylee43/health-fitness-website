0402263f5560e9a4fb5250c38ab8f4db1690024c
Jeffrey Lee <jlee52@stevens.edu>
2021-05-12 00:13:28 -0400

    get journal implementation

--- client/src/components/Journal.js
+++ client/src/components/Journal.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import '../App.css';
 import AddNoteModal from './AddNoteModal';
+import { AuthContext } from '../firebase/Auth';
+import axios from 'axios';
 
 function Journal() {
     const [modal, setModal] = useState(false);
+    const [jData, setJData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const {currentUser} = useContext(AuthContext);
 
     const handleCloseModal = () => {
         setModal(false);
@@ -11,47 +17,90 @@ function Journal() {
     const handleOpenModal = () => {
         setModal(true);
     }
-    return (
-        <div>
-            <h1 className="intro-text">Personal Journal</h1>
-            <div className="btn-group">
-                <div className="btn-div">
-                    <button className="journal-btn-style" onClick={handleOpenModal}>+ Add Workout Note</button>
-                </div> 
-                <div className="btn-div">
-                    <button className="journal-btn-style">+ Add Diet Note</button>
-                </div> 
-                <div className="btn-div">
-                    <button className="journal-btn-style">+ Add Other Note</button>
-                </div> 
-            </div>
+    let items=[];
 
-            {modal && (
-                <AddNoteModal isOpen={modal} handleClose={handleCloseModal} modal="workoutNote"/>
-            )}
-            
-            {/* <form>
-                <div>
-                    <p>Add a note to your journal:</p>
-                    <label htmlFor="subject">Choose a Subject:</label>
-                    <br/>
-                    <select name="subject" id="subject">
-                        <option value="workout">Workout</option>
-                        <option value="diet">Diet</option>
-                        <option value="other">Other</option>
-                    </select>
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const {data} = await axios.get(`/journal/${currentUser.email}`);
+                setJData(data);
+                setLoading(false);
+            } catch(e) {
+                console.log(e);
+                setError(true);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [modal]);
+
+    if(jData) {
+        items = jData.map((note)=> {
+            return (
+                <div className="note-style" key={note._id}>
+                    <p>Exercise Name: {note.exerciseName}</p>
+                    <p>Number of sets: {note.sets}</p>
+                    <p>Number of reps: {note.reps}</p>
+                    <p>Date: {note.date}</p>
                 </div>
-                <div>
-                    <label htmlFor="text">Description:</label>
-                    <br/>    
-                    <textarea className="large-input" name="text" type="text" placeholder="Enter description for note" required/>
+            )
+        });
+    } if(loading) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        )
+    } if(error) {
+        return(
+            <div>
+                <p>Error</p>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1 className="intro-text">Personal Journal</h1>
+                <div className="btn-group">
+                    <div className="btn-div">
+                        <button className="journal-btn-style" onClick={handleOpenModal}>+ Add Workout Note</button>
+                    </div> 
+                    <div className="btn-div">
+                        <button className="journal-btn-style">+ Add Diet Note</button>
+                    </div> 
+                    <div className="btn-div">
+                        <button className="journal-btn-style">+ Add Other Note</button>
+                    </div> 
                 </div>
+
+                {modal && (
+                    <AddNoteModal isOpen={modal} handleClose={handleCloseModal} modal="workoutNote"/>
+                )}
                 <div>
-                    <button className="btn-style" type="submit">Login</button>
-                </div>        
-            </form> */}
-        </div>
-    )
+                    {items.length === 0 ? <p>No notes in journal</p>: items}
+                </div>
+                {/* <form>
+                    <div>
+                        <p>Add a note to your journal:</p>
+                        <label htmlFor="subject">Choose a Subject:</label>
+                        <br/>
+                        <select name="subject" id="subject">
+                            <option value="workout">Workout</option>
+                            <option value="diet">Diet</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="text">Description:</label>
+                        <br/>    
+                        <textarea className="large-input" name="text" type="text" placeholder="Enter description for note" required/>
+                    </div>
+                    <div>
+                        <button className="btn-style" type="submit">Login</button>
+                    </div>        
+                </form> */}
+            </div>
+    )}
 }
 
 export default Journal;
--- server/routes/journal.js
+++ server/routes/journal.js
@@ -44,38 +44,19 @@ router.post('/addWorkoutNote', async (req,res) => {
     
 });
 
-// router.get('/', async (req,res) => {
-//     let {email, exerciseName, comments, sets, reps, date} = req.body;
+router.get('/:email', async (req,res) => {
+    let email = req.params.email;
+    if(!email || email==="" || email.trim() === "") {
+        return res.status(400).json({error: "You must provide all fields."});
+    }
 
-//     if(!email || email==="" || email.trim() === "") {
-//         return res.status(400).json({error: "You must provide all fields."});
-//     }
-//     if(!exerciseName || exerciseName==="" || exerciseName.trim() === "") {
-//         return res.status(400).json({error: "You must provide all fields."});
-//     }
-//     if(!comments || comments==="" || comments.trim() === "") {
-//         return res.status(400).json({error: "You must provide all fields."});
-//     }
-//     if(!sets || sets==="" || sets.trim() === "") {
-//         return res.status(400).json({error: "You must provide all fields."});
-//     }
-//     if(!reps || reps==="" || reps.trim() === "") {
-//         return res.status(400).json({error: "You must provide all fields."});
-//     }
-//     if(!date || date==="" || date.trim() === "") {
-//         return res.status(400).json({error: "You must provide all fields."});
-//     }
-
-//     const parsedSets = parseInt(sets);
-//     const parsedReps = parseInt(reps);
-
-//     try {
-//         const addNote = await journal.addWorkoutNote(email, exerciseName, comments, parsedSets, parsedReps, date);
-//         return res.sendStatus(200);
-//     } catch(e) {
-//         return res.status(400).json({error: e});
-//     }
+    try {
+        const getJournal = await journal.getJournal(email);
+        return res.send(getJournal);
+    } catch(e) {
+        return res.status(400).json({error: e});
+    }
     
-// });
+});
 
 module.exports = router;
