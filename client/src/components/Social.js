9a9d23c1d42fabe58c389fd5d39a75e2fc6a5cb0
Adam Farid <afarid@stevens.edu>
2021-05-13 23:11:09 -0400

    Added markup for client/ initialized routes for posts

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 function Social() {
-    return (
-        <div>
-            <h1>Social Media Page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="intro-text">Social Media Page</h1>
+      <p className="intro-text">Share and discover workout routines!</p>
+      <br />
+      <button className="social-btn-style">+ Create a post</button>
+    </div>
+  );
 }
 
 export default Social;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,15 @@
-const usersRoute = require('./users');
-const journalRoutes = require('./journal');
+const usersRoute = require("./users");
+const journalRoutes = require("./journal");
+const postsRoutes = require("./posts");
 
 const constructorMethod = (app) => {
-    app.use('/users', usersRoute);
-    app.use('/journal', journalRoutes);
+  app.use("/users", usersRoute);
+  app.use("/journal", journalRoutes);
+  app.use("/posts", postsRoutes);
 
-    app.use('*', (req, res) => {
-        res.status(404).json({ error: 'Provided route is not found' });
-    });
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Provided route is not found" });
+  });
 };
 
 module.exports = constructorMethod;
